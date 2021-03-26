96890def1f22b0b6ff46a2327563676a1deea63e
yiqi <yiqi.zhao@aelf.io>
2021-03-26 17:50:00 +0800

    Add view methods.

--- chain/contract/AElf.Contracts.Report/ReportContract_View.cs
+++ chain/contract/AElf.Contracts.Report/ReportContract_View.cs
@@ -1,15 +1,26 @@
+using AElf.Types;
 using Google.Protobuf.WellKnownTypes;
 
 namespace AElf.Contracts.Report
 {
     public partial class ReportContract
     {
+        public override Report GetReport(GetReportInput input)
+        {
+            return State.ReportMap[input.ObserverAssociationAddress][input.RoundId];
+        }
+
         public override StringValue GetSignature(GetSignatureInput input)
         {
             return new StringValue
             {
                 Value = State.ObserverSignatureMap[input.ObserverAssociationAddress][input.RoundId][input.Address]
             };
+        }
+
+        public override OffChainAggregatorContractInfo GetOffChainAggregatorContractInfo(Address input)
+        {
+            return State.OffChainAggregatorContractInfoMap[input];
         }
     }
 }
