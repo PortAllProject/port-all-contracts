0420344d92a6b4fbe473ee3035c3a31066a25370
李炜 <liwei@liweideMBP.lan>
2021-04-01 16:28:31 +0800

    modify report generator

--- chain/contract/ReportGenerator/Program.cs
+++ chain/contract/ReportGenerator/Program.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using AElf;
+using AElf.Contracts.Report;
 using AElf.CSharp.Core;
 using AElf.Sdk.CSharp;
-using AElf.Types;
 using Google.Protobuf;
 using Google.Protobuf.WellKnownTypes;
 
@@ -15,7 +15,7 @@ namespace ReportGenerator
     {
         static void Main(string[] args)
         {
-            Test1();
+            //Test1();
         }
 
         static void Test1()
@@ -27,98 +27,107 @@ namespace ReportGenerator
                 digestBytes[i] = 7;
             }
             var digest = ByteString.CopyFrom(digestBytes);
-            var observers = new byte[] {0, 1, 2, 3, 4};
-            var observations = new [] { -123, 245, -13213123, 123214214124421, 1};
-            var report = rs.GenerateEthereumReport(digest, 16, 16, observers, observations);
-            report = report.Substring(2, report.Length - 2);
-            //Console.WriteLine(report.Length);
-            var loopCount = report.Length / 64;
-            var total = loopCount;
-            while (loopCount > 0)
+            var data = new StringValue()
             {
-                var sub = report.Substring((total - loopCount) * 64, 64);
-                //Console.WriteLine(sub);
-                loopCount--;
-            }
-
-            report =
-                "0000000000000000000000070707070707070707070707070707070000001010000102030400000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000600000000000000000000000000000000000000000000000000000000000000005000000000000000000000000000000000000000000000000000000000000007b00000000000000000000000000000000000000000000000000000000000000f50000000000000000000000000000000000000000000000000000000000c99dc3000000000000000000000000000000000000000000000000000070100b76d3850000000000000000000000000000000000000000000000000000000000000001";
-            loopCount = report.Length / 64;
-            total = loopCount;
-            //Console.WriteLine("========= real");
-            while (loopCount > 0)
+                Value = "asdas"
+            };
+            var report = new Report
             {
-                var sub = report.Substring((total - loopCount) * 64, 64);
-                //Console.WriteLine(sub);
-                loopCount--;
-            }
-            var hashBytes = ByteStringHelper.FromHexString(report);
-            var shaHash = HashHelper.ComputeFrom(hashBytes.ToByteArray());
-            Console.WriteLine(shaHash);
+                RoundId = 10, AggregatedData = data.ToByteString(), Observations = new Observations()
+            };
+            report.Observations.Value.Add(new Observation
+            {
+                Key = "0",
+                Data = ByteString.Empty
+            });
+            report.Observations.Value.Add(new Observation
+            {
+                Key = "4",
+                Data = ByteString.Empty
+            });
+            report.Observations.Value.Add(new Observation
+            {
+                Key = "10",
+                Data = ByteString.Empty
+            });
+            var bytesInEthereum = rs.GenerateEthereumReport(digest, report);
+            //Console.WriteLine(bytesInEthereum);
         }
     }
 
     public interface IReportService
     {
-        string GenerateEthereumReport(ByteString configDigest, uint epoch, byte round, byte[] observer, long[] observation);
-        string SerializeReport(string dataTypeStr, object[] data);
+        string GenerateEthereumReport(ByteString configDigest, Report report);
     }
     
     public class ReportService: IReportService
     {
         public const string ArraySuffix = "[]";
         public const string Bytes32 = "bytes32";
-        public const string Int192 = "int192";
+        public const string Uint256 = "uint256";
         public const int SlotByteSize = 32;
 
-        private readonly Dictionary<string, Func<object, string>> serizalization;
+        private readonly Dictionary<string, Func<object, string>> _serialization;
 
         public ReportService()
         {
-            serizalization = new Dictionary<string, Func<object, string>>
+            _serialization = new Dictionary<string, Func<object, string>>
             {
-                [Bytes32] = ConvertBytes32, [Int192] = ConvertLong
+                [Bytes32] = ConvertBytes32, [Uint256] = ConvertLong
             };
         }
-
-        public string GenerateEthereumReport(ByteString configDigest, uint epoch, byte round, byte[] observer,
-            long[] observation)
+        
+        public string GenerateEthereumReport(ByteString configDigest, Report report)
         {
-            var configText = GenerateConfigText(configDigest, epoch, round);
-            var observerIndex = GenerateObserver(observer);
             var data = new object[3];
-            data[0] = configText;
-            data[1] = observerIndex;
-            data[2] = observation;
-            return SerializeReport("bytes32,bytes32,int192[]", data);
+            data[0] = GenerateConfigText(configDigest, report);
+            data[1] = GenerateObserverIndex(report);
+            Console.WriteLine(ConvertBytes32(data[1]));
+            data[2] = GenerateObservation(report.AggregatedData);
+            return SerializeReport(data, Bytes32, Bytes32, Bytes32);
         }
-
-        public const long MaxEpoch = 4294967296;
-        public byte[] GenerateConfigText(ByteString configDigest, uint epoch, byte round)
+        public byte[] GenerateConfigText(ByteString configDigest, Report report)
         {
-            if (epoch > MaxEpoch)
+            long round = report.RoundId;
+            byte observerCount = (byte)report.Observations.Value.Count;
+            byte validBytesCount = (byte) report.AggregatedData.ToByteArray().Length;
+            if (round < 0)
             {
-                throw new AssertionException("invalid epoch");
+                throw new AssertionException("invalid round");
             }
             var configText = new byte[SlotByteSize];
             var digestBytes = configDigest.ToByteArray();
-            Buffer.BlockCopy(digestBytes, 0, configText, 11, 16);
-            var epochBytes = epoch.ToBytes();
-            Buffer.BlockCopy(epochBytes, 0, configText, 27, 4);
-            configText[SlotByteSize.Sub(1)] = round;
+            Buffer.BlockCopy(digestBytes, 0, configText, 6, 16);
+            var roundBytes = round.ToBytes();
+            Buffer.BlockCopy(roundBytes, 0, configText, 22, 8);
+            configText[SlotByteSize - 2] = observerCount;
+            configText[SlotByteSize - 1] = validBytesCount;
             return configText;
         }
 
-        public byte[] GenerateObserver(byte[] observer)
+        public byte[] GenerateObservation(ByteString result)
         {
+            var observation = result.ToByteArray();
+            if (observation.Length == SlotByteSize)
+                return observation;
+            var ret = new byte[SlotByteSize];
+            Buffer.BlockCopy(observation, 0, ret, 0, observation.Length);
+            return ret;
+        }
+
+        public byte[] GenerateObserverIndex(Report report)
+        {
+            var observations = report.Observations.Value;
             var observerIndex = new byte[SlotByteSize];
-            Buffer.BlockCopy(observer, 0, observerIndex, 0, observer.Length);
+            for (var i = 0; i < observations.Count; i++)
+            {
+                observerIndex[i] =  (byte)int.Parse(observations[i].Key);
+            }
             return observerIndex;
         }
         
-        public string SerializeReport(string dataTypeStr, object[] data)
+        public string SerializeReport(object[] data, params string[] dataType)
         {
-            var dataType = dataTypeStr.Split(',');
             var dataLength = (long)dataType.Length;
             if(dataLength != data.Length)
                 throw new AssertionException("invalid data length");
@@ -128,7 +137,7 @@ namespace ReportGenerator
             for (int i = 0; i < dataLength; i++)
             {
                 var typeStrLen = dataType[i].Length;
-                if (String.CompareOrdinal(dataType[i].Substring(typeStrLen.Sub(2), 2), ArraySuffix) == 0)
+                if (string.CompareOrdinal(dataType[i].Substring(typeStrLen.Sub(2), 2), ArraySuffix) == 0)
                 {
                     var typePrefix = dataType[i].Substring(0, typeStrLen.Sub(2));
                     var dataList = data[i] as IEnumerable<long>;
@@ -140,7 +149,7 @@ namespace ReportGenerator
                     lazyData.Append(ConvertArray(typePrefix, dataList));
                     continue;
                 }
-                result.Append(serizalization[dataType[i]](data[i]));
+                result.Append(_serialization[dataType[i]](data[i]));
             }
 
             result.Append(lazyData);
@@ -171,12 +180,12 @@ namespace ReportGenerator
 
         public string ConvertArray(string dataType, IEnumerable<long> dataList)
         {
-            if (dataType != Int192)
+            if (dataType != Uint256)
                 return string.Empty;
             var dataBytes = new StringBuilder();
             foreach (var data in dataList)
             {
-                dataBytes.Append(serizalization[dataType](data));
+                dataBytes.Append(_serialization[dataType](data));
             }
 
             return dataBytes.ToString();
