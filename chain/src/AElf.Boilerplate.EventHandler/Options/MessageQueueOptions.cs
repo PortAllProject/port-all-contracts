60d4e0d2298782d7dd58ab1f28f0f76389737df5
yiqi <yiqi.zhao@aelf.io>
2021-05-13 11:03:50 +0800

    Tune MessageQueueOptions

--- chain/src/AElf.Boilerplate.EventHandler/Options/MessageQueueOptions.cs
+++ chain/src/AElf.Boilerplate.EventHandler/Options/MessageQueueOptions.cs
@@ -2,7 +2,6 @@ namespace AElf.Boilerplate.EventHandler
 {
     public class MessageQueueOptions
     {
-        public bool Enable { get; set; } = true;
         public string HostName { get; set; } = "127.0.0.1";
         public int Port { get; set; } = 5672;
         public string ClientName { get; set; } = "AElf";
