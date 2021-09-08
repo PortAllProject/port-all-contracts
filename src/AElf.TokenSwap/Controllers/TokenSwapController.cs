c0d33bf60edf5f317776555998dec7427242db02
yiqi <yiqi.zhao@aelf.io>
2021-09-08 20:21:40 +0800

    Tune CurrentPeriodStartTimestamp

--- src/AElf.TokenSwap/Controllers/TokenSwapController.cs
+++ src/AElf.TokenSwap/Controllers/TokenSwapController.cs
@@ -149,7 +149,8 @@ namespace AElf.TokenSwap.Controllers
                 var periodAward = new PeriodAward();
                 periodAward.MergeFrom(ByteString.CopyFrom(ByteArrayHelper.HexStringToByteArray(resultAward)));
                 tokenSwapInfo.CurrentPeriodId = Math.Min(periodAward.PeriodId, 7);
-                tokenSwapInfo.CurrentPeriodStartTimestamp = periodAward.StartTimestamp.ToString().Replace("\"", "");
+                tokenSwapInfo.CurrentPeriodStartTimestamp = periodAward.StartTimestamp.ToDateTime().AddHours(8)
+                    .ToString("yyyy-MM-dd HH:mm:ss");
             }
 
             {
