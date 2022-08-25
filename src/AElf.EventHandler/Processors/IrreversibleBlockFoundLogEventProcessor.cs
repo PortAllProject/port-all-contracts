c97d95fc726cae4d9875b2bc5b9d19a5fb7d906e
zhxymh <sean.zhang@aelf.io>
2022-08-25 17:50:37 +0800

    fix: fix chain id

--- src/AElf.EventHandler/Processors/IrreversibleBlockFoundLogEventProcessor.cs
+++ src/AElf.EventHandler/Processors/IrreversibleBlockFoundLogEventProcessor.cs
@@ -14,15 +14,18 @@ public class IrreversibleBlockFoundLogEventProcessor : LogEventProcessorBase<Irr
 {
     private readonly ITransmitTransactionProvider _transmitTransactionProvider;
     private readonly IAElfClientService _aelfClientService;
+    private readonly AElfChainAliasOptions _chainAliasOptions;
     public ILogger<IrreversibleBlockFoundLogEventProcessor> Logger { get; set; }
 
     public IrreversibleBlockFoundLogEventProcessor(
         IOptionsSnapshot<AElfContractOptions> contractAddressOptions,
-        ITransmitTransactionProvider transmitTransactionProvider, IAElfClientService aelfClientService) : base(
+        ITransmitTransactionProvider transmitTransactionProvider, IAElfClientService aelfClientService,
+        IOptionsSnapshot<AElfChainAliasOptions> chainAliasOptions) : base(
         contractAddressOptions)
     {
         _transmitTransactionProvider = transmitTransactionProvider;
         _aelfClientService = aelfClientService;
+        _chainAliasOptions = chainAliasOptions.Value;
 
         Logger = NullLogger<IrreversibleBlockFoundLogEventProcessor>.Instance;
     }
@@ -36,7 +39,8 @@ public class IrreversibleBlockFoundLogEventProcessor : LogEventProcessorBase<Irr
         Logger.LogInformation($"IrreversibleBlockFound: {libFound}");
 
         var chainId = ChainIdProvider.GetChainId(context.ChainId);
-        var block = await _aelfClientService.GetBlockByHeightAsync(chainId,libFound.IrreversibleBlockHeight);
+        var clientAlias = _chainAliasOptions.Mapping[chainId];
+        var block = await _aelfClientService.GetBlockByHeightAsync(clientAlias,libFound.IrreversibleBlockHeight);
         await _transmitTransactionProvider.SendByLibAsync(chainId, block.BlockHash, block.Header.Height);
     }
 }
